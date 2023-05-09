504e84bba7848fd282950ea791878e6593efe288
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-05-08 23:07:49 -0400

    Addition of another example with grandparent.

--- javascript/example/type.ts
+++ javascript/example/type.ts
@@ -19,3 +19,18 @@ type TypeEnum2 = {
 
 export type Ordinals2 = TypeEnum2[Names2]
 export type Names2 = keyof TypeEnum2
+
+type TypeEnum3 = {
+    A: 0
+    B: 1
+    C: 2
+    D: 3
+    E: 4
+    F: 5
+    G: 6
+    H: 7
+    I: 8
+}
+
+export type Ordinals3 = TypeEnum3[Names3]
+export type Names3 = keyof TypeEnum3
