c3fd10bceacde10073a362af9fbea8a63031da42
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-05-28 23:48:37 -0400

    Change on the Enumerable definition to better represent the changes

--- javascript/src/util/enumerable/Enumerable.ts
+++ javascript/src/util/enumerable/Enumerable.ts
@@ -1,16 +1,16 @@
-import type {EnumerableName, EnumerableToPrimitive}           from "enumerable/Enumerable.types"
-import type {Nullable, PossiblePrimitiveHint, PossibleString} from "../../type"
+import type {EnumerableName, EnumerableToPrimitive} from "enumerable/Enumerable.types"
 
 export interface Enumerable<ORDINAL extends number = number, NAME extends string = string, > {
 
     /**
      * Get the name on the current enum instance (not to be confused with the class name)
      *
-     * @failAtConstruction
+     * @failWhenCalledDuringTheConstruction
+     * @throws {NullReferenceException} The instance has been called during the construction or was not found in the {@link BasicCompanionEnumDeclaration.values companion values}
      */
     get name(): NAME
 
-    /** Get the ordinal on the current enum instance. */
+    /** Get the ordinal on the current instance. */
     get ordinal(): ORDINAL
 
     /**
@@ -19,10 +19,10 @@ export interface Enumerable<ORDINAL extends number = number, NAME extends string
      * If it receive a <b>null</b>, then a {@link TypeError} will be thrown.
      *
      * @param value The value (either "string", "number" or "default")
-     * @throws TypeError
+     * @throws {NullPointerException} The hint received was <b>null</b>
      * @see https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/toPrimitive
      */
-    [Symbol.toPrimitive]<HINT extends PossiblePrimitiveHint, >(value: Nullable<HINT>,): EnumerableToPrimitive<HINT, this>
+    [Symbol.toPrimitive]<const HINT extends string, >(value: Nullable<HINT>,): EnumerableToPrimitive<HINT, this>
 
     /**
      * Convert the {@link Enumerable} to a {@link String} or {@link Number}.
@@ -30,7 +30,8 @@ export interface Enumerable<ORDINAL extends number = number, NAME extends string
      * If it receive a <b>null</b> or a value that is not {@link PossiblePrimitiveHint a hint}, then a {@link TypeError} will be thrown.
      *
      * @param value The value (either "string", "number" or "default") as an object or a primitive
-     * @throws TypeError
+     * @throws {NullPointerException} The hint received was <b>null</b>
+     * @throws {ClassCastException} The hint was not a {@link PossiblePrimitiveHint} (in any casing)
      * @see https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/toPrimitive
      */
     [Symbol.toPrimitive](value: Nullable<PossibleString>,): EnumerableToPrimitive<PossiblePrimitiveHint, this>
