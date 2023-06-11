b81d668f51037caa07f65ad625a5f8ea73027357
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-06-10 23:36:19 -0400

    Simple formatting on the characters to be formatted with a single instead of double quote

--- javascript/test/collection/constantCollections.ts
+++ javascript/test/collection/constantCollections.ts
@@ -1,13 +1,14 @@
 import {GenericCollectionHolder} from "../../src/util/collection/GenericCollectionHolder"
 
 export const EMPTY = new GenericCollectionHolder([],),
-    AB = new GenericCollectionHolder(["a", "b",],),
-    A_NULL_B_UNDEFINED = new GenericCollectionHolder(["a", null, "b", undefined,],),
-    AB12 = new GenericCollectionHolder(["a", "b", 1, 2,],),
-    ABCD = new GenericCollectionHolder(["a", "b", "c", "d",],),
+    AB = new GenericCollectionHolder(['a', 'b',],),
+    AB_OBJECT = new GenericCollectionHolder([new String('a',), new String('b',),],),
+    A_NULL_B_UNDEFINED = new GenericCollectionHolder(['a', null, 'b', undefined,],),
+    AB12 = new GenericCollectionHolder(['a', 'b', 1, 2,],),
+    ABCD = new GenericCollectionHolder(['a', 'b', 'c', 'd',],),
     ABCDEFGHIJ = new GenericCollectionHolder(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',],),
-    ABCD_ABCD = new GenericCollectionHolder(["a", "b", "c", "d", "A", "B", "C", "D",],),
-    NULL_ABCD = new GenericCollectionHolder([null, "a", "b", "c", "d",],),
-    ABCD_NULL = new GenericCollectionHolder(["a", "b", "c", "d", null,],),
-    UNDEFINED_ABCD = new GenericCollectionHolder([undefined, "a", "b", "c", "d",],),
-    ABCD_UNDEFINED = new GenericCollectionHolder(["a", "b", "c", "d", undefined,],)
+    ABCD_ABCD = new GenericCollectionHolder(['a', 'b', 'c', 'd', 'A', 'B', 'C', 'D',],),
+    NULL_ABCD = new GenericCollectionHolder([null, 'a', 'b', 'c', 'd',],),
+    ABCD_NULL = new GenericCollectionHolder(['a', 'b', 'c', 'd', null,],),
+    UNDEFINED_ABCD = new GenericCollectionHolder([undefined, 'a', 'b', 'c', 'd',],),
+    ABCD_UNDEFINED = new GenericCollectionHolder(['a', 'b', 'c', 'd', undefined,],)
