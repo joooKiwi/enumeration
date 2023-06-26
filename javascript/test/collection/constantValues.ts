8b396f701d3d4758a41729335850aa9d92cb8c22
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-06-26 18:53:13 -0400

    Change on the constants to be declared by a callback instead of just once in order to have truly independent tests

--- javascript/test/collection/constantValues.ts
+++ javascript/test/collection/constantValues.ts
@@ -8,14 +8,14 @@
 import {GenericCollectionHolder} from "../../src/util/collection/GenericCollectionHolder"
 import {Holder}                  from "../Holder"
 
-export const sizeValues = [
+export const sizeValues = () => [
         new Holder({collection: new GenericCollectionHolder([],), size: 0,}, '0',),
         new Holder({collection: new GenericCollectionHolder([1,],), size: 1,}, '1',),
     ] as const,
-    templateItems = [
+    templateItems = () => [
         1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j',
         true, false,
     ] as const,
     nonPresentItem = Symbol(),
-    templateCollection = new GenericCollectionHolder(templateItems,)
+    templateCollection = new GenericCollectionHolder(templateItems(),)
