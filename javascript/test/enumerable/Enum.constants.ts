601bae87186d942fb598a2dec7f382572e230c39
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-08-03 01:26:47 -0400

    Change on the array.flat() to use an array creation directly instead. By that, there are more values validated and it is more declarative.

--- javascript/test/enumerable/Enum.constants.ts
+++ javascript/test/enumerable/Enum.constants.ts
@@ -10,6 +10,12 @@ import {EnumConstants} from "../../src/EnumConstants"
 import type {EmptyEnumWithVariables} from "../TemplateEnums"
 import {Holder}                      from "../Holder"
 
+const MAX_VALUE_AS_BIG_INT = EnumConstants.MAX_VALUE_AS_BIG_INT,
+    MIN_SAFE_NUMBER_VALUE_AS_BIG_INT = BigInt(Number.MIN_SAFE_INTEGER,),
+    MAX_SAFE_NUMBER_VALUE_AS_BIG_INT = BigInt(Number.MAX_SAFE_INTEGER,),
+    MIN_NUMBER_VALUE_AS_BIG_INT = BigInt(-Number.MAX_VALUE,),
+    MAX_NUMBER_VALUE_AS_BIG_INT = BigInt(Number.MAX_VALUE,)
+
 export const nullValues = [null, undefined,] as const,
     unhandledValues = [
         new Holder(true, "boolean",),
@@ -41,50 +47,107 @@ export const nullValues = [null, undefined,] as const,
         new Holder(new WeakSet(), "empty weak set",),
         new Holder(new WeakMap(), "empty weak map",),
     ] as const,
-    // bigIntMinValue = BigInt(Number.MIN_VALUE,),
-    bigIntMaxValue = BigInt(Number.MAX_VALUE,),
     impossibleOrdinals = [
-        ([-1,] as const).map(it => [
-            new Holder(it, `${it} as number`,),
-            new Holder(new Number(it,), `${it} as object number`,),
-        ],),
-        ([[-1, "-1",],
-            // [bigIntMinValue - 1n, "Number.MIN_VALUE - 1",], [bigIntMinValue - bigIntMinValue, "Number.MIN_VALUE * 2",],
-            [bigIntMaxValue + 1n, "Number.MAX_VALUE + 1"], [bigIntMaxValue + bigIntMaxValue, "Number.MAX_VALUE * 2",],] as const).map(([it, name,]) => [
-            new Holder(BigInt(it,), `${name} as bigint`,),
-            new Holder(Object(BigInt(it,),), `${name} as object bigint`,),
-            new Holder(it.toString(), `${name} as string`,),
-            new Holder(new String(it,), `${name} as object string`,),
-        ],),
-        ([1.1, -1.1,] as const).map(it => [
-            new Holder(it, `${it} as number`,),
-            new Holder(new Number(it,), `${it} as object number`,),
-            new Holder(it.toString(), `${it} as string`,),
-            new Holder(new String(it,), `${it} as object string`,),
-        ],),
-    ].flat(2,),
-    forbiddenNumbers = [NaN, Infinity, -Infinity,].map(it => [
-        new Holder(it, `${it} as number`,),
-        new Holder(new Number(it,), `${it} as object number`,),
-        new Holder(it.toString(), `${it} as string`,),
-        new Holder(new String(it,), `${it} as object string`,),
-    ],).flat(),
+        new Holder(-1,                                                 `-1 as number`,),
+        new Holder(new Number(-1,),                                    `-1 as object number`,),
+        new Holder(BigInt(-1,),                                        `-1 as bigint`,),
+        new Holder(Object(BigInt(-1,),),                               `-1 as object bigint`,),
+        new Holder('-1',                                               `-1 as string`,),
+        new Holder(new String(-1,),                                    `-1 as object string`,),
+
+
+        new Holder(MAX_VALUE_AS_BIG_INT + 1n,                          `"MAX_VALUE + 1" as bigint`,),
+        new Holder(Object(MAX_VALUE_AS_BIG_INT + 1n,),                 `"MAX_VALUE + 1" as object bigint`,),
+        new Holder((MAX_VALUE_AS_BIG_INT + 1n).toString(),                   `"MAX_VALUE + 1" as string`,),
+        new Holder(new String(MAX_VALUE_AS_BIG_INT + 1n,),             `"MAX_VALUE + 1" as object string`,),
+
+        new Holder(BigInt(MAX_VALUE_AS_BIG_INT * 2n,),                 `"MAX_VALUE * 2" as bigint`,),
+        new Holder(Object(BigInt(MAX_VALUE_AS_BIG_INT * 2n,),),        `"MAX_VALUE * 2" as object bigint`,),
+        new Holder((MAX_VALUE_AS_BIG_INT * 2n).toString(),                   `"MAX_VALUE * 2" as string`,),
+        new Holder(new String(MAX_VALUE_AS_BIG_INT * 2n,),             `"MAX_VALUE * 2" as object string`,),
+
+
+        new Holder(MIN_SAFE_NUMBER_VALUE_AS_BIG_INT - 1n,              `"Number.MIN_SAFE_VALUE - 1" as bigint`,),
+        new Holder(Object(MIN_SAFE_NUMBER_VALUE_AS_BIG_INT - 1n,),     `"Number.MIN_SAFE_VALUE - 1" as object bigint`,),
+        new Holder((MIN_SAFE_NUMBER_VALUE_AS_BIG_INT - 1n).toString(),       `"Number.MIN_SAFE_VALUE - 1" as string`,),
+        new Holder(new String(MIN_SAFE_NUMBER_VALUE_AS_BIG_INT - 1n,), `"Number.MIN_SAFE_VALUE - 1" as object string`,),
+
+        new Holder(MIN_SAFE_NUMBER_VALUE_AS_BIG_INT * 2n,              `"Number.MIN_SAFE_VALUE * 2" as bigint`,),
+        new Holder(Object(MIN_SAFE_NUMBER_VALUE_AS_BIG_INT * 2n,),     `"Number.MIN_SAFE_VALUE * 2" as object bigint`,),
+        new Holder((MIN_SAFE_NUMBER_VALUE_AS_BIG_INT * 2n).toString(),       `"Number.MIN_SAFE_VALUE * 2" as string`,),
+        new Holder(new String(MIN_SAFE_NUMBER_VALUE_AS_BIG_INT * 2n,), `"Number.MIN_SAFE_VALUE * 2" as object string`,),
+
+        new Holder(MIN_SAFE_NUMBER_VALUE_AS_BIG_INT + 1n,              `"Number.MAX_SAFE_VALUE + 1" as bigint`,),
+        new Holder(Object(MIN_SAFE_NUMBER_VALUE_AS_BIG_INT + 1n,),     `"Number.MAX_SAFE_VALUE + 1" as object bigint`,),
+        new Holder((MIN_SAFE_NUMBER_VALUE_AS_BIG_INT + 1n).toString(),       `"Number.MAX_SAFE_VALUE + 1" as string`,),
+        new Holder(new String(MIN_SAFE_NUMBER_VALUE_AS_BIG_INT + 1n,), `"Number.MAX_SAFE_VALUE + 1" as object string`,),
+
+        new Holder(MAX_SAFE_NUMBER_VALUE_AS_BIG_INT * 2n,              `"Number.MAX_SAFE_VALUE * 2" as bigint`,),
+        new Holder(Object(MAX_SAFE_NUMBER_VALUE_AS_BIG_INT * 2n,),     `"Number.MAX_SAFE_VALUE * 2" as object bigint`,),
+        new Holder((MAX_SAFE_NUMBER_VALUE_AS_BIG_INT * 2n).toString(),       `"Number.MAX_SAFE_VALUE * 2" as string`,),
+        new Holder(new String(MAX_SAFE_NUMBER_VALUE_AS_BIG_INT * 2n,), `"Number.MAX_SAFE_VALUE * 2" as object string`,),
+
+
+        new Holder(MIN_NUMBER_VALUE_AS_BIG_INT - 1n,                   `"Number.MIN_VALUE - 1" as bigint`,),
+        new Holder(Object(MIN_NUMBER_VALUE_AS_BIG_INT - 1n,),          `"Number.MIN_VALUE - 1" as object bigint`,),
+        new Holder((MIN_NUMBER_VALUE_AS_BIG_INT - 1n).toString(),            `"Number.MIN_VALUE - 1" as string`,),
+        new Holder(new String(MIN_NUMBER_VALUE_AS_BIG_INT - 1n,),      `"Number.MIN_VALUE - 1" as object string`,),
+
+        new Holder(MIN_NUMBER_VALUE_AS_BIG_INT * 2n,                   `"Number.MIN_VALUE * 2" as bigint`,),
+        new Holder(Object(MIN_NUMBER_VALUE_AS_BIG_INT * 2n,),          `"Number.MIN_VALUE * 2" as object bigint`,),
+        new Holder((MIN_NUMBER_VALUE_AS_BIG_INT * 2n).toString(),            `"Number.MIN_VALUE * 2" as string`,),
+        new Holder(new String(MIN_NUMBER_VALUE_AS_BIG_INT * 2n,),      `"Number.MIN_VALUE * 2" as object string`,),
+
+        new Holder(MAX_NUMBER_VALUE_AS_BIG_INT + 1n,                   `"Number.MAX_VALUE + 1" as bigint`,),
+        new Holder(Object(MAX_NUMBER_VALUE_AS_BIG_INT + 1n,),          `"Number.MAX_VALUE + 1" as object bigint`,),
+        new Holder((MAX_NUMBER_VALUE_AS_BIG_INT + 1n).toString(),            `"Number.MAX_VALUE + 1" as string`,),
+        new Holder(new String(MAX_NUMBER_VALUE_AS_BIG_INT + 1n,),      `"Number.MAX_VALUE + 1" as object string`,),
+
+        new Holder(BigInt(MAX_NUMBER_VALUE_AS_BIG_INT * 2n,),          `"Number.MAX_VALUE * 2" as bigint`,),
+        new Holder(Object(BigInt(MAX_NUMBER_VALUE_AS_BIG_INT * 2n,),), `"Number.MAX_VALUE * 2" as object bigint`,),
+        new Holder((MAX_NUMBER_VALUE_AS_BIG_INT * 2n).toString(),            `"Number.MAX_VALUE * 2" as string`,),
+        new Holder(new String(MAX_NUMBER_VALUE_AS_BIG_INT * 2n,),      `"Number.MAX_VALUE * 2" as object string`,),
+
+
+        new Holder(1.1,                                                `1.1 as number`,),
+        new Holder(new Number(1.1,),                                   `1.1 as object number`,),
+        new Holder('1.1',                                              `1.1 as string`,),
+        new Holder(new String(1.1,),                                   `1.1 as object string`,),
+
+        new Holder(-1.1,                                               `-1.1 as number`,),
+        new Holder(new Number(-1.1,),                                  `-1.1 as object number`,),
+        new Holder('-1.1',                                             `-1.1 as string`,),
+        new Holder(new String(-1.1,),                                  `-1.1 as object string`,),
+    ] as const,
+    forbiddenNumbers = [
+        new Holder(NaN,                            `"NaN" as number`,),
+        new Holder(new Number(NaN,),               `"NaN" as object number`,),
+        new Holder("NaN",                    `"NaN" as string`,),
+        new Holder(new String("NaN",),       `"NaN" as object string`,),
+
+        new Holder(Infinity,                       `"Infinity" as number`,),
+        new Holder(new Number(Infinity,),          `"Infinity" as object number`,),
+        new Holder("Infinity",               `"Infinity" as string`,),
+        new Holder(new String("Infinity",),  `"Infinity" as object string`,),
+
+        new Holder(-Infinity,                      `"-Infinity" as number`,),
+        new Holder(new Number(-Infinity,),         `"-Infinity" as object number`,),
+        new Holder("-Infinity",              `"-Infinity" as string`,),
+        new Holder(new String("-Infinity",), `"-Infinity" as object string`,),
+    ] as const,
     forbiddenInheritedMembers = EnumConstants.INHERITED_ENUMERABLE_MEMBERS.map(it => [
-        new Holder(it, `${it} as string`),
-        new Holder(new String(it,), `${it} as object string`,),
-    ]).flat(),
+        new Holder(it, `${typeof it == "symbol" ? it.description : it} as string`),
+        new Holder(new String(it,), `${typeof it == "symbol" ? it.description : it}} as object string`,),
+    ]).toArray().flat(),
     simpleEnumVariables = ["VARIABLE_STRING", "VARIABLE_NUMBER", "VARIABLE_BIGINT",] as const satisfies readonly (keyof typeof EmptyEnumWithVariables)[],
     validValues = [
-        ...([0,] as const).map(it => [
-            new Holder(it, `${it} as a number`,),
-            new Holder(new Number(it,), `${it} as an object number`,),
-            new Holder(BigInt(it,), `${it} as a bigint`,),
-            new Holder(Object(BigInt(it,),), `${it} as an object bigint`,),
-            new Holder(it.toString(), `${it} as a string number`,),
-            new Holder(new String(it,), `${it} as an object string number`,),
-        ],).flat(),
-        ...(['A',] as const).map(it => [
-            new Holder(it, `${it} as a string`,),
-            new Holder(new String(it,), `${it} as an object string`,),
-        ],).flat(),
-    ]
+        new Holder(0, `0 as a number`,),
+        new Holder(new Number(0,), `0 as an object number`,),
+        new Holder(BigInt(0,), `0 as a bigint`,),
+        new Holder(Object(BigInt(0,),), `0 as an object bigint`,),
+        new Holder('0', `0 as a string number`,),
+        new Holder(new String(0,), `0 as an object string number`,),
+
+        new Holder('A', `A as a string`,),
+        new Holder(new String('A',), `A as an object string`,),
+    ] as const
