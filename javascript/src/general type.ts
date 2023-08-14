5c4f66467960f25365940914e2551c6a40c40b89
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-08-13 23:31:00 -0400

    Change from the alias reference to be by themselves

--- javascript/src/general type.ts
+++ javascript/src/general type.ts
@@ -29,28 +29,28 @@ export type PossibleNumber<T extends number = number, > = | T | Number
 /** A simple {@link Number} in a {@link String} template */
 export type NumberTemplate<T extends number = number, > = `${T}`
 /** A possible {@link Number} (primitive, object or in a {@link String} template) */
-export type PossibleNumberOrTemplate<T extends number = number, > = | PossibleNumber<T> | NumberTemplate<T>
+export type PossibleNumberOrTemplate<T extends number = number, > = | T | Number | `${T}`
 
 /** A possible {@link BigInt} (primitive or object) */
 export type PossibleBigInt<T extends bigint = bigint, > = | T | BigInt
 /** A simple {@link BigInt} in a {@link String} template */
 export type BigIntTemplate<T extends bigint = bigint, > = `${T}`
 /** A possible {@link BigInt} (primitive, object or in a {@link String} template) */
-export type PossibleBigIntOrTemplate<T extends bigint = bigint, > = | PossibleBigInt<T> | BigIntTemplate<T>
+export type PossibleBigIntOrTemplate<T extends bigint = bigint, > = | T | BigInt | `${T}`
 
 /** A possible {@link Number} or {@link BigInt} (primitive or object) */
-export type PossibleNumeric<NUMBER extends number = number, BIG_INT extends bigint = bigint, > = | PossibleNumber<NUMBER> | PossibleBigInt<BIG_INT>
+export type PossibleNumeric<NUMBER extends number = number, BIG_INT extends bigint = bigint, > = | NUMBER | BIG_INT | Number | BigInt
 /** A possible {@link Number} or {@link BigInt} (primitive or object) in a {@link String} template */
-export type NumericTemplate<NUMBER extends number = number, BIG_INT extends bigint = bigint, > = | NumberTemplate<NUMBER> | BigIntTemplate<BIG_INT>
+export type NumericTemplate<NUMBER extends number = number, BIG_INT extends bigint = bigint, > = `${| NUMBER | BIG_INT}`
 /** A possible {@link Number} or {@link BigInt} (primitive, object or in a {@link String} template) */
 export type PossibleNumericOrTemplate<NUMBER extends number = number, BIG_INT extends bigint = bigint, >
-    = | PossibleNumberOrTemplate<NUMBER> | PossibleBigIntOrTemplate<BIG_INT>
+    = | NUMBER | BIG_INT | Number | BigInt | `${| NUMBER | BIG_INT}`
 
 /** A possible {@link String} (primitive or object) */
 export type PossibleString<T extends string = string, > = | T | String
 /** A possible {@link String}, {@link Number} or {@link BigInt} (primitive or object) */
 export type PossibleStringOrNumeric<STRING extends string = string, NUMBER extends number = number, BIG_INT extends bigint = bigint, >
-    = | PossibleString<STRING> | PossibleNumeric<NUMBER, BIG_INT>
+    = | STRING | NUMBER | BIG_INT | String | Number | BigInt
 
 /** The possible edge case {@link Number} for number conversion from a {@link String} */
 export type PossibleEdgeCaseNumericName = | "NaN" | `${| "" | "-"}Infinity`
