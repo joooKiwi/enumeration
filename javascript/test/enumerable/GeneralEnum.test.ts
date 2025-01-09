9b19177f5d25aeb11a720a4ac352900075e534de
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-01-08 23:57:26 -0500

    [JavaScript] Copyright update to 2025

--- javascript/test/enumerable/GeneralEnum.test.ts
+++ javascript/test/enumerable/GeneralEnum.test.ts
@@ -1,9 +1,14 @@
-/*******************************************************************************
- Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
-
- This project is free to use.
- All the right is reserved to the author of this project.
- ******************************************************************************/
+//··························································
+// Copyright (c) 2023-2025. Jonathan Bédard ~ JóôòKiwi
+//
+// This project is free to use.
+// All the right is reserved to the author of this project.
+// My projects:
+//  - https://github.com/joooKiwi/type
+//  - https://github.com/joooKiwi/lazy
+//  - https://github.com/joooKiwi/collection
+//  - https://github.com/joooKiwi/enumeration
+//··························································
 
 import {AnotherChildEnum, Child1Enum, Child2Enum, EmptyEnum, EmptyEnumWithVariables, Enum1, Enum2, EnumWithInheritance, EnumWithDefault, EnumWithDifferentComportment, EnumWithExcludedFields, EnumWithLateDefault, ParentEnum, Child3Enum} from "../TemplateEnums"
 import {everyStringEnumerableMember, forbiddenNumbers, impossibleOrdinals, nullValues, simpleEnumVariables, unhandledValues, validValues}                                                                                                   from "./Enum.constants"
